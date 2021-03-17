e5cf104b2b958d0edd6bd5e1110cb9a94b689378
Matt Sharp <matthew.sharp3@gmail.com>
2021-03-17 15:07:57 -0700

    formatted and corrected some of the text outputs

--- src/numgame/Main.java
+++ src/numgame/Main.java
@@ -3,29 +3,33 @@ package numgame;
 import java.util.Scanner;
 
 public class Main {
-	
-	public static void main (String[] args) {
-		final int SECRET_NUMBER = (int)(Math.random() * 100);
+
+	public static void main(String[] args) {
+		final int SECRET_NUMBER = (int) (Math.random() * 100);
 		int guesses = 0;
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("I'm thinking of a number...\nGuess:");
-		int guess = scanner.nextInt();
-		guesses ++;
-	
+
+		Scanner inputNumber = new Scanner(System.in);
+
+		System.out.print("I'm thinking of a number...\nYour guess? ");
+		int guess = inputNumber.nextInt();
+		guesses++;
+
 		while (guess != SECRET_NUMBER) {
 			if (guess < SECRET_NUMBER) {
-				System.out.print("That number is too small, guess again:\n");
-				guess = scanner.nextInt();
-				guesses ++;
-			} if (guess > SECRET_NUMBER) {
-				System.out.print("That number is too big, guess again:\n");
-				guess = scanner.nextInt();
-				guesses ++;
+				System.out.println("That number is too small, guess again:");
+				guess = inputNumber.nextInt();
+				guesses++;
 			}
-		}	if (guess == SECRET_NUMBER); {
-				System.out.println("Well done, thats the number.\nYou took " + guesses + " guesses.");
+			if (guess > SECRET_NUMBER) {
+				System.out.println("That number is too big, guess again:");
+				guess = inputNumber.nextInt();
+				guesses++;
+			}
+		}
+		if (guess == SECRET_NUMBER)
+			;
+		{
+			System.out.println("Well done, " + SECRET_NUMBER + " is the number.\nYou took " + guesses + " guesses.");
 		}
 	}
 }
